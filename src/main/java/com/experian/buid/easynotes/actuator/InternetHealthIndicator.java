8dc95a6a15af2c3e5b011faab8ca195969bf0f59
Marcos Godinho <marcos.godinho@br.experian.com>
2018-07-07 13:33:53 -0300

    Health check, info with maven data, maven eclipse-dev profile

--- src/main/java/com/experian/buid/easynotes/actuator/InternetHealthIndicator.java
+++ src/main/java/com/experian/buid/easynotes/actuator/InternetHealthIndicator.java
@@ -3,35 +3,37 @@ package com.experian.buid.easynotes.actuator;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InternetHealthIndicator implements HealthIndicator {
+	
+	private static final Log logger = LogFactory.getLog(InternetHealthIndicator.class);
 
 	@Override
 	public Health health() {
-		try {
-			if (isHostAvailable("www.google.com")) {
-				return Health.up().build();
-			}
-		} catch (IOException e) {
-			System.out.println(e);
-			return Health.down(e).build();
+		if (isHostAvailable("google.com")) {
+			return Health.up().build();
+		} else {
+			return Health.down().build();
 		}
-		return Health.down().outOfService().build();
 	}
 
-	private boolean isHostAvailable(String hostName) throws IOException {
+	private boolean isHostAvailable(String hostName) {
 		try (Socket socket = new Socket()) {
-			int port = 80;
+			int port = 443;
 			InetSocketAddress socketAddress = new InetSocketAddress(hostName, port);
 			socket.connect(socketAddress, 500);
 			return true;
-		} catch (UnknownHostException unknownHost) {
+		} catch (IOException e) {
+			if(logger.isWarnEnabled()) {
+				logger.warn("Internet is down", e);
+			}
 			return false;
 		}
 	}
